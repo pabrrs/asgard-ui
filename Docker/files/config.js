30aa50f2c9cca3542e9c30d493b32339ae788d60
Dalton Barreto <daltonmatos@sieve.com.br>
2017-04-03 14:32:31 -0300

    Dockerfile (non-optimized) funcional
    Imagem funcionando, mas temos um problema em relação ao valor
    config.apiURL. Pois esse valor é uma dependencia de build-time e não
    run-time, por isso não podemos trocar esse valor via envvar, a não ser
    que façamos um "sed inplace" no /opt/app/dist/main.js.
    
    IN-2159

---
+++ Docker/files/config.js
@@ -0,0 +1,20 @@
+
+var config = {
+  // @@ENV gets replaced by build system
+  environment: "production",
+  // If the UI is served through a proxied URL, this can be set here.
+  rootUrl: "",
+  // Defines the Marathon API URL,
+  // leave empty to use the same as the UI is served.
+  apiURL: "https://api-marathon.sieve.com.br/",
+  // Intervall of API request in ms
+  updateInterval: 5000,
+  // Local http server URI while tests run
+  localTestserverURI: {
+    address: "localhost",
+    port: 8181
+  },
+  version: "1.1.1-sievetech"
+};
+
+export default config;
