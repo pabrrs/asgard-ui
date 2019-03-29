b73b9f72a00ad0b41a07b58c798373726bc0f8e0
Lucas <lucasdomi@globo.com>
2019-03-29 17:49:20 -0300

    fix/ Retirando botao de tempo

--- src/js/asgard/components/ComponentStatsApp.jsx
+++ src/js/asgard/components/ComponentStatsApp.jsx
@@ -1,7 +1,7 @@
 import React from "react/addons";
-import {Link} from "react-router";
+// import {Link} from "react-router";
 import classNames from "classnames";
-import PopoverComponent from "../../components/PopoverComponent";
+// import PopoverComponent from "../../components/PopoverComponent";
 import OnClickOutsideMixin from "react-onclickoutside";
 
 var ComponentStatsApp = React.createClass({
@@ -32,25 +32,27 @@ var ComponentStatsApp = React.createClass({
   },
 
   render: function () {
-    var router = this.context.router;
-    var helpMenuClassName = classNames("btn btn-lg btn-default help-menu", {
-      "active": this.state.helpMenuVisible
-    });
+    // var router = this.context.router;
+    // var helpMenuClassName = classNames("btn btn-lg btn-default", {
+    //   "active": this.state.helpMenuVisible
+    // });
+    var refreshMenuClassName = classNames("btn btn-lg btn-success");
     return (
       <div style={{display: "flex",
-      flexDirection: "column", alignItems: "center"}}>
-      <h1>Stats app - 30 min</h1>
+        flexDirection: "column", alignItems: "center"}}>
+      <h1>Resources application</h1>
       <div style={{display: "flex", flexDirection: "column", alignItems: "center", marginBottom: "10px"}}>
           <span style={{fontSize: "17px"}}>CPU Usage : 50%</span>
           <span style={{fontSize: "17px"}}>RAM Usage : 40%</span>
       </div>
-      <div className={`${helpMenuClassName}`} style={{textAlign: "center", padding: "8px 16px"}}
+      <div>
+      {/* <div className={`${helpMenuClassName}`} style={{textAlign: "center", padding: "8px 16px", marginTop: "20px"}}
           onClick={this.toggleHelpMenu}>
-        <span style={{color: "white", backgroundColor: "transparent"}}>Time config</span>
+        <span style={{color: "white", backgroundColor: "transparent", marginRight: "10px"}}>Time config</span>
         <span className="caret"></span>
         <PopoverComponent visible={this.state.helpMenuVisible} dropdownApps
             className="help-menu-dropdown">
-          <ul className="dropdown-menu" style={{minWidth: "120px"}}>
+          <ul className="dropdown-menu" style={{minWidth: "120px", display: "flex", flexDirection: "column" ,justifyContent: "center", alignItems: "center"}}>
             <li>
               <span>
                 30 min
@@ -68,8 +70,13 @@ var ComponentStatsApp = React.createClass({
             </li>
           </ul>
         </PopoverComponent>
+      </div> */}
+        <div className={`${refreshMenuClassName}`} style={{textAlign: "center", padding: "8px 16px", marginTop: "20px"}}
+            onClick={this.toggleHelpMenu}>
+          <span style={{color: "white", backgroundColor: "transparent"}}>Refresh</span>
         </div>
       </div>
+    </div>
     );
   }
 });
