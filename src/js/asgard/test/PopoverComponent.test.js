4a1980f39de735cd2b176c773764981be9a79b8d
Lucas Domingues <lucasdomi@gmail.com>
2019-05-08 15:17:59 -0300

    refact/ Aumentando cobertura de teste

--- src/js/asgard/test/PopoverComponent.test.js
+++ src/js/asgard/test/PopoverComponent.test.js
@@ -5,18 +5,26 @@ import React from "react/addons";
 import PopoverComponent from "../components/PopoverComponent";
 
 describe("Popover component", function () {
+
+  const test = "Test";
+
   before(function () {
     this.component = shallow(
       <PopoverComponent visible={true}
-        className="dropdown"
-      />
+        className="dropdown">
+        <li>
+          <a>
+            {test}
+          </a>
+        </li>
+      </PopoverComponent>
     );
   });
 
   it("render component", function () {
     expect(this.component
       .text()
-    ).to.equal("");
+    ).to.equal("Test");
   });
 
 });
